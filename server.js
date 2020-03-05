875cdf9876856e604e9d8b7ef5092af6c167f4af
= <craig@craigshoemaker.net>
2020-03-05 15:54:11 -0500

    Working implemenation

--- server.js
+++ server.js
@@ -1,54 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.FUNCTIONS_WORKER_PORT;
+const PORT = process.env.FUNCTIONS_HTTPWORKER_PORT;
 
-app.post("/api/echo", (req, res) => {
+app.post("/echo", (req, res) => {
   res.json({ value: req.body });
 });
 
-app.post("/api/order", (req, res) => {
+app.post("/order", (req, res) => {
+  const message = req.body.Data.req.Body;
+  const response = {
+    Outputs: {
+      message: message,
+      res: {
+        statusCode: 200,
+        body: "Order complete"
+      }
+    }
+  };
 
-   var outputBindings = {};
-   outputBindings["message"] = "hello queue out1";
-   var invocationLogs = [
-     "invocation log1",
-     "invocation log2",
-     "invocation log3"
-   ];
-   var invocationResponse = {
-     Outputs: outputBindings,
-     Logs: invocationLogs,
-     res: "Thanks for the help!",
-     ReturnValue: "hello queue out"
-   };
-
-   console.log(JSON.stringify(invocationResponse));
-
-   res.json(invocationResponse);
-
-  // const message = { ...req.body };
-  // const response = {
-  //   Outputs: {
-  //     message: message,
-  //     res: {
-  //       statusCode: 200,
-  //       body: "Order complete"
-  //     }
-  //   }
-  // };
-
-  // // this is http worker's response to the 
-  // // host about the function invocation
-  // // the body is taken by the host and that 
-  // // becomes the3 results of the fuction invoation
-  // res.json(response);
+  // This is HTTP worker's response to the function
+  // host about the function invocation.
+  // The body is taken by the host and that
+  // becomes the results of the function invocation
+  res.json(response);
 });
 
-const server = app.listen(PORT, 'localhost', () => {
-   console.log(`Your port is ${PORT}`);
-   const { address: host, port } = server.address();
-   console.log(`Example app listening at http://${host}:${port}`);
+const server = app.listen(PORT, "localhost", () => {
+  console.log(`Your port is ${PORT}`);
+  const { address: host, port } = server.address();
+  console.log(`Example app listening at http://${host}:${port}`);
 });
