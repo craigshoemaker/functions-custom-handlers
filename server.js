9433a95ff7f6bb07c3b123c62bffcb1204ec919f
pragnagopa <pgopa@microsoft.com>
2020-03-05 11:47:20 -0800

    fixes

--- server.js
+++ server.js
@@ -3,48 +3,30 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.FUNCTIONS_WORKER_PORT;
+const PORT = process.env.FUNCTIONS_HTTPWORKER_PORT;
 
-app.post("/api/echo", (req, res) => {
+app.post("/echo", (req, res) => {
   res.json({ value: req.body });
 });
 
-app.post("/api/order", (req, res) => {
+app.post("/order", (req, res) => {
 
-   var outputBindings = {};
-   outputBindings["message"] = "hello queue out1";
-   var invocationLogs = [
-     "invocation log1",
-     "invocation log2",
-     "invocation log3"
-   ];
-   var invocationResponse = {
-     Outputs: outputBindings,
-     Logs: invocationLogs,
-     res: "Thanks for the help!",
-     ReturnValue: "hello queue out"
+    const message = { ...req.body };
+   const response = {
+     Outputs: {
+       message: message,
+       res: {
+         statusCode: 200,
+         body: "Order complete"
+       }
+     }
    };
-
-   console.log(JSON.stringify(invocationResponse));
-
-   res.json(invocationResponse);
-
-  // const message = { ...req.body };
-  // const response = {
-  //   Outputs: {
-  //     message: message,
-  //     res: {
-  //       statusCode: 200,
-  //       body: "Order complete"
-  //     }
-  //   }
-  // };
 
   // // this is http worker's response to the 
   // // host about the function invocation
   // // the body is taken by the host and that 
   // // becomes the3 results of the fuction invoation
-  // res.json(response);
+   res.json(response);
 });
 
 const server = app.listen(PORT, 'localhost', () => {
